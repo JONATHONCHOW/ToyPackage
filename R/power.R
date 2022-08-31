edc63babd73c23a9630c9a96833f2bd21e91b0f7
JONATHONCHOW <jonathonchow23@gmail.com>
2022-08-31 22:00:24 +0800

    Add R code and LICENSE

---
+++ R/power.R
@@ -0,0 +1,27 @@
+#' @title Square a number
+#'
+#' @description Takes in any numeric value and squares it.
+#'
+#' @param x A numeric value to be squared.
+#'
+#' @return The square of input.
+#' @export
+#'
+#' @examples square(2)
+square <- function(x){
+  return(x^2)
+}
+
+#' @title Cube a number
+#'
+#' @description Takes in any numeric value and cubes it.
+#'
+#' @param x A numeric value to be cubed.
+#'
+#' @return The cube of input.
+#' @export
+#'
+#' @examples cube(3)
+cube <- function(x){
+  return(x^3)
+}
